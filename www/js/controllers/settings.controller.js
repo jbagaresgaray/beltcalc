b1b1849122af31b949ba4b5501f47dc47502bcd9
Philip Cesar Garay <philipgaray2@gmail.com>
2016-06-01 11:07:22 +0800

    Add a setting for using metric or standard units

--- www/js/controllers/settings.controller.js
+++ www/js/controllers/settings.controller.js
@@ -11,11 +11,24 @@
                 }];
 
                 $scope.data = {
-                    clientSide: localStorageService.get('isResult') ? localStorageService.get('isResult') : 'step'
+                    clientSide: localStorageService.get('isResult') ? localStorageService.get('isResult') : 'step',
+                    clientSide2: localStorageService.get('isMeasure') ? localStorageService.get('isMeasure') : 'metric'
                 };
+
+                $scope.clientSideList2 = [{
+                    text: 'Metric Units',
+                    value: 'metric'
+                },{
+                    text: 'Standard Units',
+                    value: 'standard'
+                }];
 
                 $scope.serverSideChange = function(item){
                 	localStorageService.set('isResult', $scope.data.clientSide);
+                };
+
+                $scope.serverSideChange2 = function(item){
+                	localStorageService.set('isMeasure', $scope.data.clientSide2);
                 };
             }
         ]);
